e088ed99fb77ca217f5a20f71cd8cc6f3c3942ed
Casper Hougaard <casperhgrd@gmail.com>
2014-04-14 22:55:19 +0200

    Did some more of the tutorial Now a step 13 in ex1

--- NotepadCodeLab/NotepadCodeLab/Notepadv1Project/src/main/java/com/android/demo/notepad1/Notepadv1.java
+++ NotepadCodeLab/NotepadCodeLab/Notepadv1Project/src/main/java/com/android/demo/notepad1/Notepadv1.java
@@ -16,16 +16,22 @@
 
 package com.android.demo.notepad1;
 
-import android.app.Activity;
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
 import com.android.demo.notepadv1.R;
 
 public class Notepadv1 extends ListActivity {
+
+    public static final int ADDNOTE_ID = Menu.FIRST;
+
     private int mNoteNumber = 1;
+    private NotesDbAdapter mDbHelper;
+
 
     /** Called when the activity is first created. */
     @Override
@@ -33,18 +39,47 @@ public class Notepadv1 extends ListActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.notepad_list);      //Set the view to be used
-
+        mDbHelper = new NotesDbAdapter(this);       //Create instance of adapter to DB
+        mDbHelper.open();                           //Open or Create a DB
+        fillData();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
-        return super.onCreateOptionsMenu(menu);
+        boolean blnReturn = super.onCreateOptionsMenu(menu);
+        menu.add(0, ADDNOTE_ID, 0, R.string.menu_insert);
+        return blnReturn;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        return super.onOptionsItemSelected(item);
+        boolean blnResult = super.onOptionsItemSelected(item);
+
+        switch(item.getItemId())
+        {
+            case ADDNOTE_ID:
+                createNote();
+                blnResult = true;
+        }
+        return blnResult;
     }
+
+    private void createNote() {
+        String noteName = "Note " + mNoteNumber++;
+        mDbHelper.createNote(noteName, "body number: " + mNoteNumber);
+        fillData();
+    }
+
+    private void fillData() {
+        Cursor crsDbCursor = mDbHelper.fetchAllNotes();
+        startManagingCursor(crsDbCursor);
+
+        String[] from = new String[] { NotesDbAdapter.KEY_TITLE};       //Where do we want our resources from?
+        int[] to = new int[] {R.id.text1};                              //What (in the view) do we want to "bind" our data to
+
+        SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.notes_row, crsDbCursor, from, to);
+        setListAdapter(notes);
+    }
+
+
 }
